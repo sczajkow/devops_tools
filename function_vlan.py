0610c46e89b8d5aec06b1cf303156c9c5857f8ab
sczajkow <121776283+sczajkow@users.noreply.github.com>
2023-01-10 11:23:57 -0500

    Add files via upload
    Added functionality to check for a range of vlans. Also moved the get_vlan function around to better run through all the switches

--- function_vlan.py
+++ function_vlan.py
@@ -5,42 +5,49 @@ from colorama import Fore
 username = "labuser"
 password = "labuser"
 output = ""
-#qrack = list(range(2,9))
-#rack_switches = list(range(1,4))
-qrack = list(range(2,4)) #single host test variable
-rack_switches = list(range(1,2)) #single host test variable
-row = ('a','r')
-print(Fore.RED + "For a single VLAN Just enter the VLAN number")
-print("For a range of VLANs enter VLAN,VLAN" + Fore.RESET)
+qrack = list(range(2,9))
+rack_switches = list(range(1,4))
+#qrack = list(range(2,4)) # testing range single host test variable
+#rack_switches = list(range(1,2)) # testing range single host test variable
+rows = ('q','r')
+
 
 def get_vlan():
-    for rack in qrack:
-        for index in rack_switches:
-            host = f'ful01-130-q{rack}-tor-{index}.devit.ciscolabs.com'
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            try:
-                ssh.connect(host, username=username, password=password, timeout=10)
-            except:
-                if ssh.get_transport() is None:
-                    failed_host.append(host)
-                continue
-            ssh_stdin, ssh_stdout, ssh_stderr=ssh.exec_command('show inter brief')
-            output = ssh_stdout.readlines()
-            ssh.close()
-            print(Fore.GREEN + host + Fore.RESET )
-            for lines in output:
-                if lines.startswith('Eth') and not lines.startswith('Ethernet'):
-                    lines = lines.rstrip().split()
-                    lines = lines[0:2]
-                    eth = lines[0]
-                    vlan = lines[1]
-                    port_vlan[vlan] = port_vlan.get(vlan, eth)
-                    switch_vlan[vlan] = switch_vlan.get(vlan, f'q{rack}-tor-{index}')
+    for row in rows:
+        for rack in qrack:
+            for index in rack_switches:
+                if (f'{row}{rack}' == 'r5') or (f'{row}{rack}' == 'r8'):
+                    continue
+                else:
+                    host = f'ful01-130-{row}{rack}-tor-{index}.devit.ciscolabs.com'
+                    ssh = paramiko.SSHClient()
+                    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                    try:
+                        ssh.connect(host, username=username, password=password, timeout=10)
+                    except:
+                        if ssh.get_transport() is None:
+                            failed_host.append(host)
+                        continue
+                    ssh_stdin, ssh_stdout, ssh_stderr=ssh.exec_command('show inter brief')
+                    output = ssh_stdout.readlines()
+                    ssh.close()
+                    print(Fore.GREEN + host + Fore.RESET )
+                    for lines in output:
+                        if lines.startswith('Eth') and not lines.startswith('Ethernet'):
+                            lines = lines.rstrip().split()
+                            lines = lines[0:2]
+                            eth = lines[0]
+                            vlan = lines[1]
+                            port_vlan[vlan] = port_vlan.get(vlan, eth)
+                            switch_vlan[vlan] = switch_vlan.get(vlan, f'q{rack}-tor-{index}')
+                            dup_vlan[vlan] = dup_vlan.get(vlan, 0) + 1
 
 failed_host = []
 port_vlan = dict()
 switch_vlan = dict()
+dup_vlan = dict()
+
+
 
 def failed_hosts():
     print(Fore.RED + 'These hosts are either down or do no exist in the rack. Please check devit-rackman.cisco.com')
@@ -76,11 +83,15 @@ def find_single_vlan():
         if v == input_vlan:
             host = h
     if vlan is None:
-        print('This Vlan is Not Assigned')
+        print('This Vlan is Not Assigned, or in a down switch')
     else:
         print("{:<10} {:<10} {:5}".format(host,port,vlan))
+    failed_hosts()
 
 def find_range_vlan():
+    host = None
+    vlan = None
+    port = None
     get_vlan()
     frv = input_vlan.split(',')
     r1 = int(frv[0])
@@ -96,11 +107,14 @@ def find_range_vlan():
             if v == vr:
                 host = h
         if vlan is None:
-            print('This Vlan is Not Assigned')
+            print('This Vlan is Not Assigned, or in a down switch')
         else:
             print("{:<10} {:<10} {:5}".format(host,port,vlan))
+    failed_hosts()
 
-input_vlan = input('Enter Vlan or range of Vlans to Check for: ')
+print(Fore.RED + "For a single VLAN Just enter the VLAN number")
+print("For a range of VLANs enter VLAN,VLAN" + Fore.RESET)
+input_vlan = input('Enter Vlan or range of VLANs to Check for: ')
 input_vlan = input_vlan.lower()
 pattern = re.compile(r"(\d+),(\d+)")
 if input_vlan == 'all':
